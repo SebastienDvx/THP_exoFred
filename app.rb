5d9667d17ab6ff0a9bcc0dcf09bab524082c6a2f
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-23 17:49:35 +0100

    App3.rb

--- app.rb
+++ app.rb
@@ -1,29 +1,33 @@
 10.times { |n| puts "#{n+1}"}
 
-# for n in 1..10
-#   n = puts "hi world"
-# end
-#
-# n=1
-# while n < 10
-#   puts "salut monde"
-#   n += 1
-# end
-#
-# m = 1
-# until m > 10
-#   puts "holly world"
-#   m += 1
-# end
-#
-#
-# (0..10).each { puts 'hello'}
-#
-# def is_prime(nb)
-#   nb.times do |n|
-#     if nb % n == 0
-#   end
-#
-# end
-#
-# is_prime(5)
+for n in 1..10
+  n = puts "hi world"
+end
+
+n=1
+while n < 10
+  puts "salut monde"
+  n += 1
+end
+
+m = 1
+until m > 10
+  puts "holly world"
+  m += 1
+end
+
+
+(0..10).each { puts 'hello'}
+
+def is_prime(nb)
+  nb.times do |n|
+    n = n+1
+    while nb % n != 0
+      nb % n
+      n += 1
+    end
+  end
+
+end
+
+is_prime(5)
---
+++ app3.rb
@@ -0,0 +1,4 @@
+arr = ["Seb", "Aldo", "Hamza", "Raf", "Fred"]
+arr.each {|n| puts "Hi #{n} ! "  }
+
+# hash = Hash.new
